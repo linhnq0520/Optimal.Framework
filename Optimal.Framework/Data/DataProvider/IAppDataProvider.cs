c625c62e3e61809b511a0c4838460f6254d9beb8
linhnq <linhnq@jits.com.vn>
2024-01-03 16:30:54 +0700

    update

--- Optimal.Framework/Data/DataProvider/IAppDataProvider.cs
+++ Optimal.Framework/Data/DataProvider/IAppDataProvider.cs
@@ -3,8 +3,11 @@
     public interface IAppDataProvider
     {
         IQueryable<TEntity> GetTable<TEntity>() where TEntity : BaseEntity;
+
         Task<TEntity> InsertEntity<TEntity>(TEntity entity) where TEntity : BaseEntity;
+
         Task UpdateEntity<TEntity>(TEntity entity) where TEntity : BaseEntity;
+
         Task DeleteEntity<TEntity>(TEntity entity) where TEntity : BaseEntity;
     }
 }
--- Optimal.Framework/Data/IRepository.cs
+++ Optimal.Framework/Data/IRepository.cs
@@ -3,10 +3,15 @@
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
         IQueryable<TEntity> Table { get; }
+
         Task<IEnumerable<TEntity>> GetAllAsync();
+
         Task<TEntity> GetByIdAsync(int id);
+
         Task InsertAsync(TEntity entity);
+
         Task UpdateAsync(TEntity entity);
+
         Task DeleteAsync(int id);
     }
 }
